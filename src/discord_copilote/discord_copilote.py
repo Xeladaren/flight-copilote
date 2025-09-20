368b07c9535072aedafc53a5c23f50e556f8faa9
Xeladaren <xeladaren@pm.me>
2025-09-20 15:19:07 +0200

    add service file, add args parser

--- src/discord_copilote/discord_copilote.py
+++ src/discord_copilote/discord_copilote.py
@@ -1,12 +1,18 @@
 
 import json
+import argparse
+
 from .airport_db import AirportDB
 from .discord_bot import DiscordCopilote
 
 def main():
     print("Hello, Discord Copilote!")
 
-    with open("config.json", "r") as f:
+    parser = argparse.ArgumentParser(description="Discord Copilote Bot")
+    parser.add_argument("--config", type=str, default="config.json", help="Path to the configuration file")
+    args = parser.parse_args() 
+
+    with open(args.config, "r") as f:
         config = json.load(f)
 
     airport_db = AirportDB(api_token=config["airport-db"]["apiToken"])
